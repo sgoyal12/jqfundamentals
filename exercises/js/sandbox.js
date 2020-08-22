9206de74900a40815143279d3dfa66b07e3afca7
sgoyal12 <shu12.sg@gmail.com>
2020-08-22 21:30:52 +0530

    third solution traversing

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -6,17 +6,13 @@ $("img").each(function() {
 });
 
 //Select the search input text box, then traverse up to the form and add a class to the form.
-$("input.input_text").parent().addClass("abc");
+$("input.input_text").parent().addClass("hint");
 
 //Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item.
-let listItem = $("#myList .current");
-listItem.removeClass("current");
-listItem.next().addClass("current");
+$("#myList li.current").removeClass("current").next().addClass("current");
 
 //Select the select element inside #specials; traverse your way to the submit button.
-$("#specials select").parents("form").find("input[type='submit']");
+$("#specials select").closest("form").find("input[type='submit']");
 
 //Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
-let firstItem = $("#slideshow li").first();
-firstItem.addClass("current");
-firstItem.nextAll().addClass("disabled");
+$("#slideshow li:first").addClass("current").nextAll().addClass("disabled");
