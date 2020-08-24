97fa6a588e1d45a7c86eeb3abf184d13dc4048ab
sgoyal12 <shu12.sg@gmail.com>
2020-08-24 15:14:27 +0530

    hidden text 3 solution

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -8,15 +8,13 @@ class RevealText {
   }
 
   bindClickTolist() {
-    this.blogList.bind("click", function(event) {
-      event.preventDefault(); 
-      let h3Element = $(event.target.closest("h3"));
-      if(h3Element) {
-        let excerpt = h3Element.next(),
-            visibleExcerpt = h3Element.parents("ul").find("p:visible");
-        visibleExcerpt.slideUp();    
-        excerpt.slideDown();
-      }
+    this.blogList.delegate("h3", "click", function(event) {
+      event.preventDefault();
+      let h3Element = $(event.target.closest("h3")),
+          excerpt = h3Element.next(),
+          visibleExcerpt = h3Element.parents("ul").find("p:visible");
+      visibleExcerpt.slideUp();    
+      excerpt.slideDown();
     });
   }
 }
