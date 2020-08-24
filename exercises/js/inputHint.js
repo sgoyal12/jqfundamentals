c3562eea459f33e434f64c811116e480245f6bfe
sgoyal12 <shu12.sg@gmail.com>
2020-08-24 21:00:35 +0530

    3 solution

--- exercises/js/inputHint.js
+++ exercises/js/inputHint.js
@@ -1,50 +1,50 @@
-class Hint {
+class SearchHandler {
   constructor(options) {
     this.parentForm = $(options.formSelector);
+    this.className = options.className;
     this.inputField = this.parentForm.find(options.inputSelector);
     this.labelField = this.parentForm.find(options.labelSelector);
   }
 
   init() {
-    this.removeLabelFeild();
+    this.removeLabelField();
     this.bindEventHandlers();
   }
 
-  removeLabelFeild() {
+  removeLabelField() {
     // Set the value of the search input to the text of the label element
-    let labelText = this.labelField.text();
-    this.inputField.data("labelText", labelText);
-    this.inputField.val(labelText);
     // Add a class of "hint" to the search input
-    this.inputField.addClass("hint");
+    this.setLabelText();
     // Remove the label element
     this.labelField.remove();
   }
 
+  setLabelText() {
+    const labelText = this.labelField.text();
+    this.inputField.val(labelText);
+    this.inputField.addClass(this.className);
+  }
+
+  removeLabelText() {
+    this.inputField.val("");
+    this.inputField.removeClass(this.className);
+  }
+
   bindEventHandlers() {
     // Bind a focus event to the search input that removes the hint text and the "hint" class
-    this.inputField.focus(this.focusEventHandler);
+    this.inputField.focus(this.removeLabelText.bind(this));
     // Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
-    this.inputField.blur(this.blurEventHandler);
+    this.inputField.blur(this.setLabelText.bind(this));
 
-  }
-
-  focusEventHandler() {
-    let element = $(this);
-    element.val("");
-    element.removeClass("hint");
-  }
-
-  blurEventHandler() {
-    let element = $(this);
-    element.val(element.data("labelText"));
-    element.addClass("hint");
   }
 }
 $(function() {
-  let options = { inputSelector: "[data-input-hint='input-field']",
-                  labelSelector: "[data-input-hint-label='input-label-field']",
-                  formSelector: "[data-input-hint-form='hint-form']" };
-  let hint = new Hint(options);
-  hint.init();
+  let options = { 
+    inputSelector: "[data-input-hint='input-field']",
+    labelSelector: "[data-input-hint-label='input-label-field']",
+    formSelector: "[data-input-hint-form='hint-form']",
+    className: "hint"
+  };
+  let searchHandler = new SearchHandler(options);
+  searchHandler.init();
 });
