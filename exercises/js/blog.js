5f30c345b7f4aa682afc7a4e1cba423c0dd89af2
sgoyal12 <shu12.sg@gmail.com>
2020-08-24 21:24:54 +0530

    4th solution

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -1,25 +1,23 @@
 class RevealText {
   constructor(options) {
-    this.blogList = $(options.listSelector);
+    this.blogList = options.list;
   }
 
   init() {
-    this.bindClickTolist();
+    this.bindClickToList();
   }
 
-  bindClickTolist() {
-    this.blogList.delegate("h3", "click", function(event) {
+  bindClickToList() {
+    this.blogList.bind("click", function(event) {
       event.preventDefault();
-      let h3Element = $(event.target.closest("h3")),
-          excerpt = h3Element.next(),
-          visibleExcerpt = h3Element.parents("ul").find("p:visible");
-      visibleExcerpt.slideUp();    
-      excerpt.slideDown();
+      let h3Element = $(this);
+      h3Element.parents("ul").find("p:visible").slideToggle();
+      h3Element.next().slideToggle();
     });
   }
 }
 $(document).ready(() => {
-  const options = { listSelector: "[data-id='blog'] ul" },
+  let options = { list: $("[data-id='blog'] h3") },
       revealText = new RevealText(options);
   revealText.init();
 });
