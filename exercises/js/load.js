38b40eca26199324a5bdf7b1d8b3698b4503f91b
sgoyal12 <shu12.sg@gmail.com>
2020-08-25 20:20:53 +0530

    Done the Changes

--- exercises/js/load.js
+++ exercises/js/load.js
@@ -1,43 +1,52 @@
 // Create a target div after the headline for each blog post and store a reference to it on the headline element using $.fn.data.
 // Bind a click event to the headline that will use the $.fn.load method to load the appropriate content from /exercises/data/blog.html into the target div. Don't forget to prevent the default action of the click event.
-class LoadHtml {
+class ContentLoader {
 	constructor(options) {
-		this.container = $(options.containerSelector);
-    this.listOfItems = this.container.children("ul");
+    this.options = options;
+		this.listOfItems = $(options.listSelector);
 	}
 
   init() {
     this.addDivForContent();
-    this.addClickHandler();
+    this.listOfItems.bind("click", this.listItemClickEventHandler.bind(this));
   }
 
   addDivForContent() {
     let parent = this.listOfItems.parent();
     this.listOfItems.detach();
-    this.listOfItems.children("li").each(function() {
-      let item = $(this),
-          div = $("<div></div>"),
-          id = item.find("a").attr("href").split("#")[1];
-      div.insertAfter(item.children("[data-content-div-after='true']"));
-      item.data({ "contentDiv": div,
-                  "contentId": "#" + id });
-    });
+    this.listOfItems.children("li").each(this.createDivForListItem.bind(this));
     parent.append(this.listOfItems);
   }
 
-  addClickHandler() {
-    this.listOfItems.bind("click", function(event) {
-      let target = $(event.target.closest("li"));
-      if(target) {
-        event.preventDefault();
-        let post = target.data("contentId");
-        target.data("contentDiv").load("data/blog.html " + post);
-      }
+  createDivForListItem(index, item) {
+    let $item = $(item),
+        div = $("<div></div>"),
+        id = $item.find("a").attr("href");
+    div.insertAfter($item.children(this.options.addDivAfterSelector));
+    $item.data({ 
+      "contentDiv": div,
+      "contentId": id
     });
+  }
+
+  listItemClickEventHandler(event) {
+    let target = $(event.target.closest("li"));
+    if(target) {
+      event.preventDefault();
+      this.loadContentInsideDiv(target);
+    }
+  }
+
+  loadContentInsideDiv(target) {
+    let post = target.data("contentId");
+    target.data("contentDiv").load("data/" + post);
   }
 }
 $(document).ready(() => {
-  const options = { containerSelector: "[data-load-blog-container='container']" };
-        loadHtml = new LoadHtml(options);
-  loadHtml.init();
+  const options = { 
+    listSelector: "[data-load-blog-list='list']",
+    addDivAfterSelector: "[data-content-div-after='true']"
+  };
+  let contentLoader = new ContentLoader(options);
+  contentLoader.init();
 });
