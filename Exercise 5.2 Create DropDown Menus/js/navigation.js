b6a390f522227330522a335089e9400cb747960b
sgoyal12 <shu12.sg@gmail.com>
2020-08-26 21:53:50 +0530

    removed hoverEventHandler, removed children method

--- Exercise 5.2 Create DropDown Menus/js/navigation.js
+++ Exercise 5.2 Create DropDown Menus/js/navigation.js
@@ -1,42 +1,35 @@
 class DropDownMenu {
   constructor(options) {
+    this.options = options
     this.mainMenu = $(options.mainMenuSelector);
-    this.menuItems = this.mainMenu.children(options.menuItemSelector);
+    this.menuItems = this.mainMenu.find(options.menuItemSelector);
   }
 
   init() {
-    this.menuItems.hover(this.hoverEventHandler.bind(this));
+    this.menuItems.hover(this.hoverInEventHandler.bind(this),
+                         this.hoverOutEventHandler.bind(this));
   }
 
-  hoverEventHandler(event) {
+  hoverInEventHandler() {
     let target = $(event.target);
-    let subMenu = target.children("ul");
-    if(event.type === "mouseenter") {
-      this.hoverInEventHandler(target, subMenu);
-    }
-    else {
-      this.hoverOutEventHandler(target, subMenu);
-    }
+    let subMenu = target.find(this.options.subMenuItemSelector);
+    target.addClass(this.options.hoverClass);
+    subMenu.show();
   }
 
-  hoverInEventHandler(target, subMenu) {
-    target.addClass("hover");
-    if(subMenu.length) {
-      subMenu.show();
-    }
-  }
-
-  hoverOutEventHandler(target, subMenu) {
-    target.removeClass("hover");
-    if(subMenu.length) {
-      subMenu.hide();
-    }
+  hoverOutEventHandler() {
+    let target = $(event.target);
+    let subMenu = target.find(this.options.subMenuItemSelector);
+    target.removeClass(this.options.hoverClass);
+    subMenu.hide();
   }
 }
 $(document).ready(() => {
   let options = {
     mainMenuSelector: "[data-nav-main-menu='main_menu']",
-    menuItemSelector: "[data-nav-menu-item='item']"
+    menuItemSelector: "[data-nav-menu-item='item']",
+    subMenuItemSelector: "[data-nav-sub-menu-item='item']",
+    hoverClass: "hover"
   };
   let dropDownMenu = new DropDownMenu(options);
   dropDownMenu.init();
